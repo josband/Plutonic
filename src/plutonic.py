322de9377d66a063d50d32f18fc6ba388172ab13
Jonah Osband <79324007+josband@users.noreply.github.com>
2023-07-02 17:58:09 -0400

    Further linting fixes

--- src/plutonic.py
+++ src/plutonic.py
@@ -2,6 +2,7 @@
 Plutonic Functionality. Everything needed for automating trades are here.
 """
 
+import sys
 from alpaca.trading.client import TradingClient
 import utils.logger
 
@@ -9,7 +10,7 @@ class Plutonic:
     """
     Plutonic Trading Bot. This is the main interface in which to operate it
     """
-    
+
     def __init__(self, api_key=None, secret_key=None) -> None:
         utils.logger.info('Starting Plutonic Initialization')
         try:
@@ -17,6 +18,12 @@ class Plutonic:
             utils.logger.success('Successfully Connected Trading Client')
         except ValueError:
             utils.logger.error("Could Not Successfully Load API Client")
-            exit(1)
+            sys.exit(1)
 
         utils.logger.success('Initialization Complete')
+
+    def execute_buy() -> None:
+        pass
+
+    def execute_sell() -> None:
+        pass
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,38 +2,37 @@
 Functionailty needed for logging
 """
 
-class Color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m' # back to default
+PURPLE = '\033[95m'
+CYAN = '\033[96m'
+DARKCYAN = '\033[36m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+END = '\033[0m' # back to default
 
 def info(*args) -> None:
     """
     For loggging normal information
     """
-    print(f'[{Color.BOLD}INFO{Color.END}]:', *args)
+    print(f'[{BOLD}INFO{END}]:', *args)
 
 def success(*args) -> None:
     """
     For loggging successful operations
     """
-    print(f'[{Color.GREEN + Color.BOLD}SUCCESS{Color.END}]:', *args)
+    print(f'[{GREEN + BOLD}SUCCESS{END}]:', *args)
 
 def warning(*args) -> None:
     """
     For loggging any warnings
     """
-    print(f'[{Color.YELLOW + Color.BOLD}WARNING{Color.END}]:', *args)
+    print(f'[{YELLOW + BOLD}WARNING{END}]:', *args)
 
 def error(*args) -> None:
     """
     For loggging failures
     """
-    print(f'[{Color.RED + Color.BOLD}ERROR{Color.END}]:', *args)
+    print(f'[{RED + BOLD}ERROR{END}]:', *args)
